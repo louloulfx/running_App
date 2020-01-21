aa892056ba10932363c983d5e69abec1a1daaf5a
Valentin Guibert <valentinguibert@hotmail.fr>
2020-01-21 14:36:39 +0100

    better style for button

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -55,9 +55,9 @@ export default class LocationComponent extends Component {
 
         <View style={styles.overlay} >
           <TouchableNativeFeedback style={styles.button} onPress={this._onPressButton}>
-          <View style={styles.button}>
-          <Text>RUN</Text>
-          </View>
+            <View style={styles.button}>
+              <Text style={{ color: 'white', fontSize: 20 }} >RUN</Text>
+            </View>
           </TouchableNativeFeedback>
         </View>
 
@@ -97,22 +97,30 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: '#8bc34a',
     display: 'flex',
-    alignItems:'center',
-    justifyContent:'center',
-    width:300,
-    height:50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 300,
+    height: 50,
     color: '#000',
     zIndex: 2,
     borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
 
+    elevation: 5,
   },
   overlay: {
     position: 'absolute',
     zIndex: 1,
     display: 'flex',
-    alignItems:'center',
-    justifyContent:'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   }
