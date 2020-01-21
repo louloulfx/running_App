8c962826443ea5e3263176d8c1954d141d68023c
Valentin Guibert <valentinguibert@hotmail.fr>
2020-01-21 11:22:56 +0100

    clean homepage style

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,8 @@ import {
   Platform,
   StyleSheet,
   View,
-  Dimensions 
+  Dimensions,
+  Button
 } from 'react-native';
 
 import LocationComponent from '../components/MapComponent';
@@ -12,7 +13,13 @@ import LocationComponent from '../components/MapComponent';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <LocationComponent />
+      <Button
+        onPress={() => {
+          alert('Are you ready to run ?');
+        }}
+        title="RUN"
+      />
+      {/* <LocationComponent /> */}
     </View>
   );
 }
@@ -28,47 +35,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  
 });
