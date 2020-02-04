f8167285da9dfaf1461107e9a9ab92eb7c6ea3de
Valentin Guibert <valentinguibert@hotmail.fr>
2020-02-04 18:21:07 +0100

    add second overlay for run

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -43,10 +43,6 @@ export default class LocationComponent extends Component {
     this.setState({ location });
   };
 
-  _onPressButton() {
-    alert("Are you ready to run ?");
-  }
-
   render() {
     let lat = 0;
     let long = 0;
@@ -60,7 +56,7 @@ export default class LocationComponent extends Component {
     return (
 
       <View style={styles.container}>
-      <OverlayComponent />
+        <OverlayComponent />
         <MapView
           region={{
             latitude: lat,
--- components/OverlayComponent.js
+++ components/OverlayComponent.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Platform, Text, View, StyleSheet, Dimensions, TouchableNativeFeedback } from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome";
 
-export default class OverlayComponent extends Component {
+class NotRunning extends Component {
     render() {
+
         return (
-            <View style={styles.overlay} >
-                <TouchableNativeFeedback style={styles.button} onPress={this._onPressButton}>
-                    <View style={styles.button}>
+            <View style={notRunningStyle.overlay} >
+                <TouchableNativeFeedback style={notRunningStyle.button} onPress={this.props.pushed}>
+                    <View style={notRunningStyle.button}>
                         <Text style={{ color: 'white', fontSize: 20 }} >RUN</Text>
                     </View>
                 </TouchableNativeFeedback>
@@ -15,7 +17,7 @@ export default class OverlayComponent extends Component {
     }
 }
 
-const styles = StyleSheet.create({
+const notRunningStyle = StyleSheet.create({
     button: {
         backgroundColor: '#8bc34a',
         display: 'flex',
@@ -47,4 +49,111 @@ const styles = StyleSheet.create({
         height: Dimensions.get('window').height,
     }
 })
+
+class Running extends Component {
+    render() {
+        return (
+            <View style={runningStyle.overlay} >
+                <View style={runningStyle.up}>
+                    <Text>Nb KM</Text>
+                    <Text>Timer</Text>
+                </View>
+                <View style={runningStyle.down}>
+                    <TouchableNativeFeedback onPress={this.props.pushed}>
+                        <View style={[runningStyle.button_pause, runningStyle.button]}>
+                            <Icon name="pause" size={26} color="white" />
+                        </View>
+                    </TouchableNativeFeedback>
+
+                    <TouchableNativeFeedback onPress={this.props.pushed}>
+                        <View style={[runningStyle.button_stop, runningStyle.button]}>
+                            <Icon name="square" size={26} color="white" />
+                        </View>
+                    </TouchableNativeFeedback>
+                </View>
+
+            </View>
+        );
+    }
+}
+const runningStyle = StyleSheet.create({
+    overlay: {
+        position: 'absolute',
+        zIndex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingTop: 40,
+        paddingBottom: 35,
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+    up: {
+        display: 'flex',
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    down: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        display: 'flex',
+        width: '100%',
+        alignItems:'center'
+    },
+    button: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 2,
+        borderRadius: 10,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    button_pause: {
+        backgroundColor: 'grey',
+        width: 50,
+        height: 50,
+    },
+    button_stop: {
+        backgroundColor: 'red',
+        width: 70,
+        height: 70,
+    }
+})
+
+export default class OverlayComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { isRunning: false };
+        this.stopRun = this.stopRun.bind(this)
+        this.displayRun = this.displayRun.bind(this)
+    }
+
+    stopRun() {
+        this.setState({ isRunning: false });
+        console.log("isRunning set to false");
+    };
+
+    displayRun() {
+        this.setState({ isRunning: true });
+        console.log("isRunning set to true");
+    };
+
+    render() {
+        if (this.state.isRunning) {
+            return (<Running pushed={this.stopRun} />)
+        } else {
+            return (<NotRunning pushed={this.displayRun} />)
+        }
+    }
+}
+
 
