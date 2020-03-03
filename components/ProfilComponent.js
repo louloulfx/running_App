dd5a035f6218f18eea8c1dc85e5012b2f990b6bf
CorentinGlrd5 <corentin.guillard@ynov.com>
2020-03-03 11:45:24 +0100

    feat(ProfilBack): start recover id

--- components/ProfilComponent.js
+++ components/ProfilComponent.js
@@ -4,6 +4,26 @@ import firebase from "firebase";
 import HelloComponent from "../components/model/HelloComponent";
 
 class ProfilComponent extends Component {
+  componentDidMount() {
+    const { currentUser } = firebase.auth();
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(currentUser.uid)
+      .get()
+      .then(function(doc) {
+        if (doc.exists) {
+          console.log("Document data:", doc.data());
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      })
+      .catch(function(error) {
+        console.log("Error getting document:", error);
+      });
+  }
+
   constructor(props) {
     super(props);
     this.state = {
